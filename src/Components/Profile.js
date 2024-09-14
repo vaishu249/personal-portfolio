ca76775869bfb0067477f865ada7435037a268f9
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-14 20:15:05 +0530

    Complete profile with DEsign

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,25 +2,27 @@ import React from 'react'
 import profileImg from "../assets/images/profile.jpg";
 import "./Profile.css";
 const Profile = () => {
-  return (
-    <div className='profile-wrapper'>
-        <h2>Vaisnavi Gundgire</h2>
-        <div className='profile-img'>
-            <img src={profileImg}/>
+    return (
+        <div className='profile-wrapper'>
+            <h2>Vaisnavi Gundgire</h2>
+            <div className='profile-img'>
+                <img src={profileImg} />
+            </div>
+            <div className='profile-specialization'>
+                <p>specialization</p>
+                <h3>Full Stack Developer</h3>
+            </div>
+            <div className='profile-social-link'>
+                <p>Follow me on social media</p>
+                <div className='profile-social-icons'>
+                    <i className="ri-facebook-line"></i>
+                    <i className="ri-github-fill"></i>
+                    <i className="ri-instagram-line"></i>
+                </div>
+            </div>
+            <button>let's Work Together!</button>
         </div>
-        <div className='profile-specialization'>
-            <p>specialization</p>
-            <h3>Full Stack Developer</h3>
-        </div>
-        <div className='profile-social-link'>
-            <p>Follow me on social media</p>
-            <i className="ri-facebook-line"></i>
-            <i className="ri-github-fill"></i>
-            <i className="ri-instagram-line"></i>
-        </div>
-        <button>Works Together</button>
-    </div>
-  )
+    )
 }
 
 export default Profile
