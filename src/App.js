ce1d7b8116e1d1334fd6fd554a4679ea308cf1cb
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-14 20:29:48 +0530

    Basic design for the main container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Profile from './Components/Profile';
-
+import Main from './Components/Main/Main';
 function App() {
   return (
     <>
       <Profile/>
+      <div className='main-wrapper'>
+        <Main/>
+      </div>
     </>
   );
 }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../Main/Main.css"
+const Main = () => {
+  return (
+    <>
+        <h1>main</h1>
+    </>
+  )
+}
+
+export default Main
