aa9a3c45c194f6892092d0bf65d714cba8c88508
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-14 20:59:52 +0530

    Navbar design

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import "../Main/Main.css"
 const Main = () => {
-  return (
-    <>
-        <h1>main</h1>
-    </>
-  )
+    return (
+        <>
+            <div className='navbar'>
+                <ul>
+                    <li><a href='#'><span>Home</span> <i className='ri-home-line'></i> </a></li>
+                    <li><a href='#'><span>About Me</span> <i className='ri-user-line'></i></a></li>
+                    <li><a href='#'><span>Portfolio</span><i className='ri-user-4-line'></i></a></li>
+                    <li><a href='#'><span>Resume</span><i className='ri-file-list-line'></i></a></li>
+                    <li><a href='#'><span>Contact</span><i className='ri-contacts-book-line'></i></a></li>
+                    <li id='nav_ThemeBtn'>
+                        <i className='ri-sun-line' ></i>
+                    </li>
+                </ul>
+                <div className='navbar-btn'>
+                    <i className='ri-sun-line'></i>
+                    <button>Let's Talk</button>
+                    <i className='ri-menu-3-line'></i>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default Main
