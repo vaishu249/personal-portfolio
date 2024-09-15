0068916120b15fcaddedb6cf6d073ee6bb7be330
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-15 15:33:58 +0530

    Created about folder structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Profile from './Components/Profile/Profile';
 import Main from './Components/Main/Main';
 import Header from './Components/Header/Header';
 import Portfolio from './Components/Portfolio/Portfolio';
+import About from './Components/About/About';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Main/>
         <Header/>
         <Portfolio/>
+        <About/>
       </div>
     </>
   );
---
+++ src/Components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../About/About.css"
+const About = () => {
+  return (
+    <div>
+      hiiiiiii
+    </div>
+  )
+}
+
+export default About
