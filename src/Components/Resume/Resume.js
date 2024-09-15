99f5f723374f832cdbad7c894cf10928cf9d6765
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-15 20:31:33 +0530

    Added heading

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -2,8 +2,12 @@ import React from 'react'
 import "../Resume/Resume.css"
 const Resume = () => {
   return (
-    <div>
-      hello
+    <div className='section resume-wrapper'>
+      <h3 className='section-heading'><i className='ri-shining-fill'>Resume</i></h3>
+      <h1 className='section-title'>Education and <br />Practical Experience</h1>
+      <div className='education-wrapper'>
+        <h1>Education</h1>
+      </div>
     </div>
   )
 }
