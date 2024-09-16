9a8bf4eb63f9ae3a2bbd94433ef85d855c10a11a
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-16 13:00:56 +0530

    Add education detail

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -7,6 +7,21 @@ const Resume = () => {
       <h1 className='section-title'>Education and <br />Practical Experience</h1>
       <div className='education-wrapper'>
         <h1>Education</h1>
+        <div className='Education'>
+          <h2>Jan.2022-May.2025</h2>
+          <h3>Kestone School of Engineering Pune</h3>
+          <p>Bachelor of Engineering In Computer Science - CGPA : 9.18/10</p>
+        </div>
+        <div className='Education'>
+          <h2>Jan.2019-May.2022</h2>
+          <h3>Jaywantrao Sawant Polytechnic Hadapsar Pune</h3>
+          <p>Diploma in Computer - Percentage : 86.78%</p>
+        </div>
+        <div className='Education'>
+          <h2>Jan.2018-May.2019</h2>
+          <h3>Sadhana Girls School Hadapsar Pune</h3>
+          <p>10th Class : 82.83%</p>
+        </div>
       </div>
     </div>
   )
