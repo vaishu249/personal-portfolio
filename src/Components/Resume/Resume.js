655200c46f68f0aca32d0e4a43ed7228bfbc5e9d
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-16 13:30:30 +0530

    Complete design of resume page

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -7,20 +7,48 @@ const Resume = () => {
       <h1 className='section-title'>Education and <br />Practical Experience</h1>
       <div className='education-wrapper'>
         <h1>Education</h1>
+
         <div className='Education'>
           <h2>Jan.2022-May.2025</h2>
           <h3>Kestone School of Engineering Pune</h3>
-          <p>Bachelor of Engineering In Computer Science - CGPA : 9.18/10</p>
+          <p>BE - CGPA : 9.18/10</p>
         </div>
         <div className='Education'>
           <h2>Jan.2019-May.2022</h2>
           <h3>Jaywantrao Sawant Polytechnic Hadapsar Pune</h3>
-          <p>Diploma in Computer - Percentage : 86.78%</p>
+          <p>Diploma (Computer) : 86.78%</p>
         </div>
         <div className='Education'>
           <h2>Jan.2018-May.2019</h2>
           <h3>Sadhana Girls School Hadapsar Pune</h3>
           <p>10th Class : 82.83%</p>
+        </div>
+      </div>
+
+      <div className='education-wrapper'>
+        <h1>Projects</h1>
+
+        <div className='Education'>
+          <h2>2024</h2>
+          <h3>Restaurant Website Design using React Js</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever
+            since the 1500s, when an Lorem Ipsum is simply dummy text of 
+            the printing and typesetting industry. Lorem Ipsum has 
+            been the industry's standard dummy text ever since the 1500s, 
+            when an
+          </p>
+        </div>
+        <div className='Education'>
+          <h2>2024</h2>
+          <h3>Restaurant Website Design using React Js</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever
+            since the 1500s, when an Lorem Ipsum is simply dummy text of 
+            the printing and typesetting industry. Lorem Ipsum has 
+            been the industry's standard dummy text ever since the 1500s, 
+            when an
+          </p>
         </div>
       </div>
     </div>
